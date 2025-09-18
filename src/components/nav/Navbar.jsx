689a53b36a094a3d321e21bdeadabfdff8588569
oitsjustjose <stovallj1995@gmail.com>
2025-09-17 19:39:33 -0500

    Update assets

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -7,7 +7,7 @@ export default () => (
   <nav className="navbar navbar-expand-xl fixed-top navbar-dark bg-dark">
     <div className="container-fluid mw-100">
       <a href="/" className="navbar-brand" aria-label="navbar-brand">
-        <img src="/me.png" width="30" height="30" alt="" loading="lazy" />
+        <img src="/img/favicon.png" width="30" height="30" alt="" loading="lazy" />
       </a>
       <button
         className="navbar-toggler"
